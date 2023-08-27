29b7bf8c5b742e73d2ce14620fbb3a501a1fc65a
claudioSilva11 <99560963+claudioSilva11@users.noreply.github.com>
2023-08-27 17:31:16 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask,  render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("index.html")
+
+@app.route("/estatistica-descritiva")
+def estatisticaDescritiva():
+    return render_template("estatisticaDescritiva.html")
+
+@app.route("/glossario")
+def glossario():
+    return render_template("glossario.html")
+
+@app.route("/amplitude")
+def amplitude():
+    return render_template("amplitude.html")
+
+@app.route("/variancia")
+def variancia():
+    return render_template("varianca.html")
+
+@app.route("/desvio-padrao")
+def desvio_padrao():
+    return render_template("desvio_padrao.html")
+
+@app.route("/coeficiente-variacao")
+def coeficiente_variacao():
+    return render_template("coeficiente_variacao.html")
+
+@app.route("/media")
+def media():
+    return render_template("media.html")
+
+@app.route("/mediana")
+def mediana():
+    return render_template("mediana.html")
+
+@app.route("/moda")
+def moda():
+    return render_template("moda.html")
+
+@app.route("/questoes")
+def questoes():
+    return render_template("questoes.html")
+
+app.run()
